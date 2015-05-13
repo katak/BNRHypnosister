650e01db3a3981d190c967f4b17bd4ddce3fb760
Kris Kata <kata.krzysztof@gmail.com>
2015-05-13 14:57:20 -0700

    Moved project to correct directory

---
+++ Hypnosister/Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Kris Kata on 5/13/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
