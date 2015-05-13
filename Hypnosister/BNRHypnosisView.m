01276d5c46788c8a9139d8db6a973df5add972b0
Kris Kata <kata.krzysztof@gmail.com>
2015-05-13 15:50:09 -0700

    Complete Hypnosister app

--- Hypnosister/BNRHypnosisView.m
+++ Hypnosister/BNRHypnosisView.m
@@ -33,12 +33,21 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    // The circle will be the largest that will fit in the view
-    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    // The largest circle will circumscribe the view
+    float maxRadius = hypotf(bounds.size.width, bounds.size.height) / 2.0;
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path addArcWithCenter:center radius:radius startAngle:0 endAngle:M_PI * 2 clockwise:YES];
-
+    
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0
+                      endAngle:M_PI * 2
+                     clockwise:YES];
+    }
+    
     // Configure line width to 10 points
     path.lineWidth = 10;
     
