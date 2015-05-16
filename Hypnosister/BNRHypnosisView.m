ea719a65c07caf793a2dee6287cf7269e8b97925
Kris Kata <kata.krzysztof@gmail.com>
2015-05-16 00:04:08 -0700

    Chapter 4 Gold Challenge - Shadows and Gradients

--- Hypnosister/BNRHypnosisView.m
+++ Hypnosister/BNRHypnosisView.m
@@ -57,11 +57,44 @@
     // Draw the line!
     [path stroke];
     
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+
     // Create smaller rectangle for overlay image
     CGRect overlayBounds = CGRectMake(bounds.size.width / 6.0, bounds.size.height / 6.0, bounds.size.width / 1.5, bounds.size.height / 1.75);
     
+    UIBezierPath *triangle = [[UIBezierPath alloc] init];
+    CGPoint top = CGPointMake(overlayBounds.origin.x + overlayBounds.size.width / 2.0, overlayBounds.origin.y);
+    [triangle moveToPoint:top];
+    [triangle addLineToPoint:CGPointMake(overlayBounds.origin.x, overlayBounds.origin.y + overlayBounds.size.height * 1.1)];
+    [triangle addLineToPoint:CGPointMake(overlayBounds.origin.x + overlayBounds.size.width, overlayBounds.origin.y + overlayBounds.size.height * 1.1)];
+    [triangle addLineToPoint:top];
+    [triangle stroke];
+    
+    CGContextSaveGState(currentContext);
+    [triangle addClip];
+    
+    // Create gradient
+    CGFloat locations[2] = { 0.8, 0.0 };
+    CGFloat components[8] = { 1.0, 1.0, 0.0, 1.0,   // Start color is yellow
+        0.0, 1.0, 0.0, 1.0 }; // End color is green
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 2);
+    
+    CGPoint startPoint = CGPointMake(overlayBounds.origin.x + overlayBounds.size.width / 2.0, overlayBounds.origin.y);
+    CGPoint endPoint = CGPointMake(overlayBounds.origin.x + overlayBounds.size.width / 2.0, overlayBounds.origin.y + overlayBounds.size.height * 1.1);
+    CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(currentContext);
+    
+    // Create logo with shadow
+    CGContextSaveGState(currentContext);
+    
+    CGContextSetShadow(currentContext, CGSizeMake(4,7), 3);
+    
     UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
     [logoImage drawInRect:overlayBounds];
+    
+    CGContextRestoreGState(currentContext);
 }
 
 @end
